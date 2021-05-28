5daa7018891f24d869c5d661bd088f68aaed4d3a
Matthew Clifford <mattclifford2000@gmail.com>
2021-05-28 23:51:36 +1000

    Subrouting added

--- client/src/Pages/Admin/AddProduct.tsx
+++ client/src/Pages/Admin/AddProduct.tsx
@@ -1,1 +1,9 @@
-import React from 'react'
+import React from 'react';
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
+const AddProduct = (): JSX.Element => {
+return(<div></div>)
+}
+
+export default AddProduct;
---
+++ client/src/Pages/Admin/AdminHome.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import {
+    Redirect,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import { Form, Button, Card, Badge } from "react-bootstrap";
+import AddProduct from '../Admin/AddProduct';
+
+const AdminHome = (): JSX.Element => {
+    const [url, setUrl] = useState("");
+    useEffect(() => {
+        async function setURL() {
+            if (process.env.NODE_ENV === "development") {
+                setUrl("localhost");
+            } else if (process.env.NODE_ENV === "production") {
+                setUrl("remote");
+            }
+        }
+        setURL();
+    }, [url]);
+
+    if (
+        localStorage.getItem("loggedIn") !== "true" ||
+        localStorage.getItem("role") !== "admin"
+    ) {
+        return <Redirect to="/" />;
+    }
+
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/adminhome">
+                        <Card>
+                            <Card.Header>
+                                <h3 className="form">
+                                    <Badge>Admin Home</Badge>
+                                </h3>
+                            </Card.Header>
+                            <Card.Body>
+                                <p>
+                                    Welcome to the admin home. Please click a
+                                    button to visit the task you would like to
+                                    do.
+                                </p>
+                                <Button href="/adminhome/addproduct">
+                                    Add Product
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Route>
+                    <Route path="/adminhome/addproduct">
+                        <AddProduct />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+};
+
+export default AdminHome;
